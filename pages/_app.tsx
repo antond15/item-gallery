7ea31a7166757b207169de5d6df707587c823306
ANTOND <71350868+antond15@users.noreply.github.com>
2022-09-04 14:10:09 +0200

    feat: Default structure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,19 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { MantineProvider } from '@mantine/core';
-import { theme } from './theme';
+import { theme } from '../theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Page title</title>
+        <title>Item Gallery</title>
+        <link rel="icon" href="/favicon.svg" />
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <meta
+          name="description"
+          content="Gallery of images suitable for game inventories with intended use mainly for FiveM."
+        />
       </Head>
 
       <MantineProvider withGlobalStyles withNormalizeCSS theme={theme}>
